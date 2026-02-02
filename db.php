32f7fa45a23cef399c6fc0a5e1f231be5b09170e
paljum <moneonhanabi@gmail.com>
2026-02-02 23:21:12 +0545

    Update db.php

--- db.php
+++ db.php
@@ -1,23 +1,17 @@
 <?php
-session_start(); 
 $host = "localhost";
-$user = "root";//np03cy4a240057
-$pass = "";//R81xKg6Y2O
+$user = "root";
+$pass = "";
 $dbname = "task_db";
 
-// $host = "localhost";
-// $user = " ";
-// $pass = " ";
-// $dbname = "task_db";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
-if (empty($_SESSION['csrf_token'])) {
+if (isset($_SESSION) && empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
@@ -27,7 +21,10 @@ if (isset($_GET['check_user'])) {
     mysqli_stmt_bind_param($stmt, "s", $u);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_store_result($stmt);
+    
     echo mysqli_stmt_num_rows($stmt) > 0 ? "taken" : "available";
+    
+    mysqli_stmt_close($stmt);
     exit();
 }
 ?>
